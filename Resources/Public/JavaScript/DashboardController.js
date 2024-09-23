4b0be4d1a5cbe298b2b023b0e2bc8f80f2b79e63
Matthias Richter <matthias.richter@slub-dresden.de>
2024-09-23 11:31:14 +0200

    Repeat dashboard

--- Resources/Public/JavaScript/DashboardController.js
+++ Resources/Public/JavaScript/DashboardController.js
@@ -3,19 +3,57 @@ const tx_publisherdb_dashboardExcludedYear = 'excluded-year';
 const tx_publisherdb_dashboardPublisher = 'publisher';
 
 const tx_publisherdb_dashboardController = {
-    
+
     set target(target) {
         this._target = target;
         this.render();
     },
 
-    render() {
-        const createIncludeButton = type => id => `<a id="${id}" class="include-${type} m-1"> + </a>`;
-        const createYearIncludeButton = createIncludeButton('year');
-        const createElementIncludeButton = createIncludeButton('element');
-
-        const target = d3.select(`#${this._target}`);
+    draw(targetElement) {
+        const target = d3.select(targetElement);
         target.html('');
+
+        const movingAverages = target.append('div')
+            .attr('id', 'dashboard-moving-averages')
+        movingAverages.append('h3')
+            .html('Anzeige');
+        const movingAveragesList = movingAverages.append('div')
+            .attr('class', 'tiny button-group');
+        tx_publisherdb_visualizationStatus.movingAverages.forEach( ma => {
+            movingAveragesList
+                .append('a')
+                .attr('id', `dashboard-set-moving-average-${ma}`)
+                .attr('class', 'dashboard-set-moving-average primary button hollow')
+                .html(`gleitender MW ${ma}`);
+        });
+        movingAveragesList.append('a')
+            .attr('id', 'dashboard-set-cumulative')
+            .attr('class', 'primary button hollow dashboard-set-cumulativity')
+            .html('kumulativ');
+        movingAveragesList.append('a')
+            .attr('id', 'dashboard-set-per-year')
+            .attr('class', 'primary button hollow dashboard-set-granularity')
+            .html('pro Jahr');
+        movingAveragesList.append('a')
+            .attr('id', 'dashboard-set-by-date')
+            .attr('class', 'primary button hollow dashboard-set-granularity')
+            .html('nach Datum');
+
+        if (!tx_publisherdb_visualizationStatus.isPublishedItem) {
+            const publishers = target.append('div')
+                .attr('id', 'dashboard-publishers');
+            publishers.append('h3')
+                .text('Verlage');
+            const publisherList = publishers.append('div')
+                .attr('class', 'tiny button-group');
+            publisherList.selectAll(`a.${tx_publisherdb_dashboardPublisher}`)
+                .data(tx_publisherdb_visualizationStatus.publishers)
+                .join('a')
+                .attr('class', `${tx_publisherdb_dashboardPublisher} primary button hollow include-year`)
+                .attr('id', d => d.id)
+                .html(d => d.id)
+                .attr('title', d => d.name);
+        }
 
         const excludedItems = target.append('div')
             .attr('id', 'dashboard-excluded-items');
@@ -39,54 +77,15 @@ const tx_publisherdb_dashboardController = {
                 .text('keine Verlagsartikel ausgeschlossen')
         }
 
-        /*
-        const excludedYears = target.append('div')
-            .attr('id', 'dashboard-excluded-years');
-        excludedYears.append('h3')
-            .text('Ausgeschlossene Jahre');
-        if (tx_publisherdb_visualizationStatus.excludedYears.length) {
-            const excludedYearList = excludedYears.append('div')
-                .attr('class', 'tiny button-group');
-            excludedYearList.selectAll(`a.${tx_publisherdb_dashboardExcludedYear}`)
-                .data(tx_publisherdb_visualizationStatus.excludedYears.sort())
-                .join('a')
-                .attr('class', `${tx_publisherdb_dashboardExcludedYear} primary button hollow include-year`)
-                .attr('id', d => d)
-                .html(d => d);
-        }
-        */
+    },
 
-        const movingAverages = target.append('div')
-            .attr('id', 'dashboard-moving-averages')
-        movingAverages.append('h3')
-            .html('Anzeige');
-        const movingAveragesList = movingAverages.append('div')
-            .attr('class', 'tiny button-group');
-        tx_publisherdb_visualizationStatus.movingAverages.forEach( ma => {
-            movingAveragesList
-                .append('a')
-                .attr('id', `dashboard-set-moving-average-${ma}`)
-                .attr('class', 'dashboard-set-moving-average primary button hollow')
-                .html(`gleitender MW ${ma}`);
-        });
-        movingAveragesList.append('a')
-            .attr('id', 'dashboard-set-cumulative')
-            .attr('class', 'primary button hollow dashboard-set-cumulativity')
-            .html('kumulativ');
-        /*
-        movingAveragesList.append('a')
-            .attr('id', 'dashboard-set-absolute')
-            .attr('class', 'primary button hollow dashboard-set-cumulativity')
-            .html('absolut');
-            */
-        movingAveragesList.append('a')
-            .attr('id', 'dashboard-set-per-year')
-            .attr('class', 'primary button hollow dashboard-set-granularity')
-            .html('pro Jahr');
-        movingAveragesList.append('a')
-            .attr('id', 'dashboard-set-by-date')
-            .attr('class', 'primary button hollow dashboard-set-granularity')
-            .html('nach Datum');
+    render() {
+        const createIncludeButton = type => id => `<a id="${id}" class="include-${type} m-1"> + </a>`;
+        const createYearIncludeButton = createIncludeButton('year');
+        const createElementIncludeButton = createIncludeButton('element');
+
+        const target = d3.selectAll(`.${this._target}`);
+        target.nodes().forEach(target => this.draw(target));
 
         $('a.include-year').click( e => {
             const year = e.currentTarget.id;
@@ -138,22 +137,6 @@ const tx_publisherdb_dashboardController = {
                 }
             }
         });
-
-        if (!tx_publisherdb_visualizationStatus.isPublishedItem) {
-            const publishers = target.append('div')
-                .attr('id', 'dashboard-publishers');
-            publishers.append('h3')
-                .text('Verlage');
-            const publisherList = publishers.append('div')
-                .attr('class', 'tiny button-group');
-            publisherList.selectAll(`a.${tx_publisherdb_dashboardPublisher}`)
-                .data(tx_publisherdb_visualizationStatus.publishers)
-                .join('a')
-                .attr('class', `${tx_publisherdb_dashboardPublisher} primary button hollow include-year`)
-                .attr('id', d => d.id)
-                .html(d => d.id)
-                .attr('title', d => d.name);
-        }
 
         $(`a.${tx_publisherdb_dashboardPublisher}`).click ( e => {
             tx_publisherdb_visualizationStatus.currentPublisher = e.currentTarget.id;
